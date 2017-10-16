b8ef76ba863c3ec762f75028149c0db0c6eec09e
LingWenshao <shaopson@outlook.com>
2017-10-16 22:03:28 +0800

    favicon
    favicon and footer

--- myblog/settings.py
+++ myblog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'b_2loiw_@d90!$v0tx=ac&#4psjq#r=7q)_%mkmvw3r!eb!1ic'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost','shaopson.cn','120.78.77.157']
+ALLOWED_HOSTS = ['127.0.0.1','localhost',' .shaopson.cn','120.78.77.157']
 
 
 # Application definition
